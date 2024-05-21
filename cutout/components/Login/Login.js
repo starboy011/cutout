cc7fb97048cc75016e504000159fe02bf8ad020f
starboy011 <rishavofficial294@gmail.com>
2024-05-21 20:40:26 +0100

    Starting login screen

--- cutout/components/Login/Login.js
+++ cutout/components/Login/Login.js
@@ -2,13 +2,11 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const Login = () => {
-  return (
-    <View>
-      <Text>Login </Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { height: "100%", width: "100%" },
+});
