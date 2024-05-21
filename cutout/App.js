a137deccb1f02e7b8f868c2bb462e8df9d1542a4
starboy011 <rishavofficial294@gmail.com>
2024-05-21 19:49:32 +0100

    splash screen

--- cutout/App.js
+++ cutout/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import ScissorAnimation from "./components/SplashScreen/ScissorAnimation";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
+      <ScissorAnimation />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    height: "100%",
+    width: "100%",
+  },
+});
