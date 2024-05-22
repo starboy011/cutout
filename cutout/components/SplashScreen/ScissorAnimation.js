6d81306aad0e56cf08366369401c568c8665c2fb
starboy011 <rishavofficial294@gmail.com>
2024-05-22 10:03:48 +0100

    splash done, starting login

--- cutout/components/SplashScreen/ScissorAnimation.js
+++ cutout/components/SplashScreen/ScissorAnimation.js
@@ -1,23 +1,37 @@
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  SafeAreaView,
+} from "react-native";
 import React from "react";
 
 const ScissorAnimation = () => {
   return (
-    <>
+    <SafeAreaView>
       <View style={styles.container}>
-        <ImageBackground
-          source={require("./SplashScreen.gif")}
-          style={styles.backgroundImage}
-        ></ImageBackground>
+        <View style={styles.imagecontainer}>
+          <ImageBackground
+            source={require("./SplashScreen.gif")}
+            style={styles.backgroundImage}
+          ></ImageBackground>
+        </View>
       </View>
-    </>
+    </SafeAreaView>
   );
 };
 
 export default ScissorAnimation;
 
 const styles = StyleSheet.create({
-  container: { width: "20%", height: "10%" },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  imagecontainer: { width: "20%", height: "10%" },
   backgroundImage: {
     flex: 1,
     width: "100%",
