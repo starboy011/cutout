a2513a8b11e65864eb2e6ce2bb72f4a5872bebb0
starboy011 <rishavofficial294@gmail.com>
2024-05-22 13:36:20 +0100

    added safe area

--- cutout/components/Login/LoginScreen1.js
+++ cutout/components/Login/LoginScreen1.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Platform,
+  StatusBar,
+  View,
+} from "react-native";
 import React from "react";
 
 const LoginScreen1 = () => {
   return (
-    <View>
-      <Text>LoginScreen1</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}></View>
+    </SafeAreaView>
   );
 };
 
 export default LoginScreen1;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  container: {
+    flex: 1,
+  },
+});
