11a4b1141a1d3705d5d415fa342acb03e26400aa
starboy011 <rishavofficial294@gmail.com>
2024-05-27 16:40:13 +0100

    headercontent over with animation

---
+++ cutout/components/GlobalSearch/GlobalSearch.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { Searchbar } from "react-native-paper";
+const { width, height } = Dimensions.get("window");
+const GlobalSearch = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  return (
+    <Searchbar
+      placeholder="Locate a barbershop near you"
+      onChangeText={setSearchQuery}
+      value={searchQuery}
+      iconColor="#343a40"
+      style={{
+        width: width * 0.9,
+        backgroundColor: "#e9ecef",
+      }}
+    />
+  );
+};
+
+export default GlobalSearch;
+
+const styles = StyleSheet.create({});
