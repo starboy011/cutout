066b43b255b1b3b348a437f3b5e7631b3d2858f3
starboy011 <rishavofficial294@gmail.com>
2024-05-29 12:00:38 +0100

    added offers section in the bottom tab

---
+++ cutout/components/Offers/Offers.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+const Offers = () => {
+  return (
+    <SafeAreaView>
+      <Text>Offers</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Offers;
+
+const styles = StyleSheet.create({});
