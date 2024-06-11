7c3511ccb6ce184ff9275410e2948ae33df76405
starboy011 <rishavofficial294@gmail.com>
2024-06-11 11:53:28 +0100

    starting business hours

--- cutout/components/ShopDetailsScreen/ShopDetailsScreen.js
+++ cutout/components/ShopDetailsScreen/ShopDetailsScreen.js
@@ -4,6 +4,7 @@ import BackgroundImage from "./BackgroundImage";
 import BookSlotNow from "./BookSlotNow";
 import BasicInfo from "./BasicInfo";
 import ServicesOffered from "./ServicesOffered";
+import Timings from "./Timings";
 const { width, height } = Dimensions.get("window");
 const ShopDetailsScreen = () => {
   const Services = ["Haircut", "Shave", "Trim", "Hair Color", "Beard"];
@@ -22,6 +23,9 @@ const ShopDetailsScreen = () => {
         <View style={styles.servicesOfferedContainer}>
           <ServicesOffered Services={Services} />
         </View>
+        <View style={styles.timingsContainer}>
+          <Timings />
+        </View>
       </ScrollView>
     </View>
   );
@@ -32,15 +36,21 @@ export default ShopDetailsScreen;
 const styles = StyleSheet.create({
   container: {
     width: width,
-    height: height,
+    backgroundColor: "white",
   },
   basicInfoContainer: {
     backgroundColor: "white",
     justifyContent: "center",
     alignItems: "center",
+    height: 80,
   },
   servicesOfferedContainer: {
     justifyContent: "center",
     backgroundColor: "white",
+  },
+  timingsContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "white",
   },
 });
