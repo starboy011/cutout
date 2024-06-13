a5552e0da58de09544ce2536ee7ac23242f48989
starboy011 <rishavofficial294@gmail.com>
2024-06-13 10:40:55 +0100

    added google maps functionality

--- cutout/components/ShopDetailsScreen/ImageTopBar.js
+++ cutout/components/ShopDetailsScreen/ImageTopBar.js
@@ -3,9 +3,27 @@ import React from "react";
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
-
+import * as Location from "expo-location";
+import { Linking } from "react-native";
 const ImageTopBar = () => {
   const navigation = useNavigation();
+
+  const getDirections = async () => {
+    // Request location permissions
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission to access location was denied");
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    const { latitude, longitude } = location.coords;
+    const endPoint = {
+      latitude: 51.45583409558622,
+      longitude: -0.9367586067695464,
+    };
+    const url = `https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&destination=${endPoint.latitude},${endPoint.longitude}&travelmode=driving`;
+    Linking.openURL(url);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.iconContainer}>
@@ -14,7 +32,7 @@ const ImageTopBar = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.iconContainer}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={getDirections}>
           <MaterialIcons name={"directions"} size={30} color={"white"} />
         </TouchableOpacity>
       </View>
